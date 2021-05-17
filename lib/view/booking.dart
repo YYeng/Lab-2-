c87cb68291f2fcba05d476b6e549df512a05659d
YYeng <K.Y025@hotmail.com>
2021-05-17 18:11:55 +0800

    booking interface

---
+++ lib/view/booking.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+ 
+
+class Booking extends StatefulWidget {
+  @override
+  _BookingState createState() => _BookingState();
+}
+
+class _BookingState extends State<Booking> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
