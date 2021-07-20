a49133ff34b2e41373019fa1a5f0ce875b9fa036
YYeng <K.Y025@hotmail.com>
2021-07-21 02:56:56 +0800

    payment

--- lib/view/payment.dart
+++ lib/view/payment.dart
@@ -4,11 +4,16 @@ import 'package:cv_battey/model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'booking.dart';
+
 class PayScreen extends StatefulWidget {
   final User user;
   final double total;
+  final DateTime date;
+  final String time;
 
-  const PayScreen({Key key, this.user, this.total}) : super(key: key);
+  const PayScreen({Key key, this.user, this.total, this.date, this.time})
+      : super(key: key);
 
   @override
   _PayScreenState createState() => _PayScreenState();
@@ -21,8 +26,16 @@ class _PayScreenState extends State<PayScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Booking()));
+            }),
         title: Text('Payment'),
-          flexibleSpace: Container(
+        flexibleSpace: Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
                   begin: Alignment.topCenter,
@@ -36,15 +49,20 @@ class _PayScreenState extends State<PayScreen> {
             children: [
               Expanded(
                 child: WebView(
-           // php generate bill
-                  initialUrl: 'https://crimsonwebs.com/s270737/cvbattery/generate_bill.php?email=' +
-                      widget.user.email +
-                      '&mobile=' +
-                      widget.user.phone +
-                      '&name=' +
-                      widget.user.name +
-                      '&amount=' +
-                      widget.total.toStringAsFixed(2),
+                  // php generate bill
+                  initialUrl:
+                      'https://crimsonwebs.com/s270737/cvbattery/php/generate_bill.php?email=' +
+                          widget.user.email +
+                          '&mobile=' +
+                          widget.user.phone +
+                          '&name=' +
+                          widget.user.name +
+                          '&amount=' +
+                          widget.total.toStringAsFixed(2) +
+                          '&date=' +
+                          widget.date.toString() +
+                          '&time=' +
+                          widget.time.toString(),
                   javascriptMode: JavascriptMode.unrestricted,
                   onWebViewCreated: (WebViewController webViewController) {
                     _controller.complete(webViewController);
